726c417357b41a5e854071580ab00261a136af4c
JAMVIS ROSARIO <23sc4224_ms@psu.edu.ph>
2026-05-28 06:05:39 +0800

    Add Render Blueprint deployment config

--- docker/render/start.sh
+++ docker/render/start.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+if [ -n "$RENDER_EXTERNAL_URL" ] && [ -z "$APP_URL" ]; then
+    export APP_URL="$RENDER_EXTERNAL_URL"
+fi
+
+if [ -n "$APP_KEY" ] && [[ "$APP_KEY" != base64:* ]]; then
+    export APP_KEY="base64:$APP_KEY"
+fi
+
 php artisan storage:link || true
 php artisan config:cache
 php artisan view:cache
